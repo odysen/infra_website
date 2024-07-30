0025d8b5a245b84191922668f5d16c843ed6ddfc
kerichdev <kerichuudev@gmail.com>
2024-07-30 12:35:11 +0300

    feat: deprecate primevue (main.js)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config'
-import Aura from `@primevue/themes/aura`
 import './style.css'
 import App from './App.vue'
 
-const App = createApp(App);
-App.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
+createApp(App).mount('#app')
