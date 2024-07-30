fcc436586bfcdc40fe1f6a9d9d484a799b8cb05c
kerichdev <kerichuudev@gmail.com>
2024-07-30 12:22:41 +0300

    feat: config vue to use primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from `@primevue/themes/aura`
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+const App = createApp(App);
+App.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
