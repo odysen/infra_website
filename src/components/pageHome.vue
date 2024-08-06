1e1cb5fc2bd2ffa3cff2147c185d6360e7caa7ac
kerichdev <kerichuudev@gmail.com>
2024-08-06 23:37:00 +0300

    feat: add topbar to homepage

--- src/components/pageHome.vue
+++ src/components/pageHome.vue
@@ -1,8 +1,10 @@
 <script setup>
+  import floatieBar from './floatieBar.vue'
   import Button from 'primevue/button'
 </script>
 
 <template>
+  <floatieBar></floatieBar>
   <div>
     <h1 style="font-size: 4rem">Breaking <span style="color: var(--odysen-mint)">the rules</span> of Android.</h1>
     <div style="display: flex; gap: 1rem; justify-content: center;" id="cover">
