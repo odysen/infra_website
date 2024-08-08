7a45561d4f5600173c237dea1de6aff0ee35288a
kerichdev <kerichuudev@gmail.com>
2024-08-08 15:57:34 +0300

    feat: add links and placeholder content

--- src/components/pageHome.vue
+++ src/components/pageHome.vue
@@ -8,12 +8,27 @@
   <div class="homepageSection">
     <h1 style="font-size: 4rem">Breaking <span style="color: var(--odysen-mint)">the rules</span> of Android.</h1>
     <div style="display: flex; gap: 1rem; justify-content: center;" id="cover">
-      <Button label="Discover Odysen" severity="secondary"/>
+      <Button as="a" label="Discover Odysen" href="#discover" severity="secondary"/>
       <Button label="Watch the trailer"/>
     </div>
   </div>
   <div>
+    <p id="discover"><br><br><br></p> <! -- this br stuff is CRAZY but eh, we'll figure it out -->
     <h1> What is all this about? </h1>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> This text is all here to test scrolling capabilities :3 </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
+    <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
     <p> Odysen is a blah-blah-blah. Copyrighing later, we're doing development here. I wonder if primevue has anything for text stuff? </p>
   </div>
 </template>
